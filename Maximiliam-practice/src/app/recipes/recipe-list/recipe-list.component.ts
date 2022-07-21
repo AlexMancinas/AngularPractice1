6a2e5749367c87d916ef68bb1b4a7d35389c6883
Alex Mancinas <alexman381@gmail.com>
2022-07-20 21:36:48 -0700

    avance

---
+++ Maximiliam-practice/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a test recipe title', 'This is a simply test description', 'https://images.pexels.com/photos/1640771/pexels-photo-1640771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+    new Recipe('a test recipe title', 'This is a simply test description', 'https://images.pexels.com/photos/1640771/pexels-photo-1640771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
